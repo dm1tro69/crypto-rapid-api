86acbf794ea3b75b7fff9f0cecd9a9387d46ac95
dm1tro69 <dimolg22@gmail.com>
2022-03-21 22:39:08 +0200

    add cryptocurrensies

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import 'antd/dist/antd.css'
+import {Provider} from "react-redux";
+import store from "./app/store";
 
 ReactDOM.render(
+    <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+    </Provider>,
   document.getElementById('root')
 );
 
