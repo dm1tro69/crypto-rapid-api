eb7863d7cad0e357cafda1f882e007553ccc7325
dm1tro69 <dimolg22@gmail.com>
2022-03-21 16:17:47 +0200

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import './App.css'
+import {Layout, Typography, Space} from "antd";
+import Navbar from "./components/Navbar";
+import {Link, Route, Routes} from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 
 const App = () => {
     return (
-        <div>
-          App
+        <div className={'app'}>
+          <div className={'navbar'}>
+                <Navbar/>
+          </div>
+            <div className={'main'}>
+                <Layout>
+                    <div className={'routes'}>
+                        <Routes>
+                            <Route path={'/'} element={<Homepage/>}/>
+                            <Route path={'/exchanges'} element={<Exchanges/>}/>
+                            <Route path={'/cryptocurrencies'} element={<Cryptocurrencies/>}/>
+                            <Route path={'/crypto/:coinId'} element={<CryptoDetails/>}/>
+                            <Route path={'/news'} element={<News/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
+
+            <div className={'footer'}>
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Cryptoverse <br/>
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/exchanges'}>Exchanges</Link>
+                    <Link to={'/news'}>News</Link>
+                </Space>
+            </div>
+            </div>
         </div>
     );
 };
